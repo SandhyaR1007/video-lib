e7b1131927f612e4992c1e249989ef208c746db0
Sandhya1007 <86607783+Sandhya1007@users.noreply.github.com>
2023-05-08 19:35:18 +0530

    update: styles for layout

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -4,14 +4,15 @@ import Actionbar from "./Actionbar";
 import Sidebar from "./Sidebar";
 import Home from "../../pages/Home";
 import Liked from "../../pages/Liked";
+import Categories from "./Categories";
 
 export const Layout = () => {
   return (
     <div className="layout">
       <Sidebar />
+      <Actionbar />
+      <Categories />
       <div className="layout__content">
-        <Actionbar />
-
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/liked" element={<Liked />} />
