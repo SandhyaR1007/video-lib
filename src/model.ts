f11909d3a0d52e4265e48d03a759b40287a4362f
SandhyaR1007 <grajwanshi1008@gmail.com>
2023-05-10 01:53:49 +0530

    update: interface for initialState

--- src/model.ts
+++ src/model.ts
@@ -5,4 +5,17 @@ export type SingleVideo = {
   url: string;
   thumbnail: string;
   duration: string;
+  categories: string[];
 };
+
+export interface VideoInitialStateType {
+  videosData: SingleVideo[];
+}
+export interface VideoContextType {
+  videosData: SingleVideo[];
+  loading: boolean;
+}
+export interface ActionType {
+  type: string;
+  payload: any;
+}
